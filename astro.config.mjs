0bf871cb2a05f126acd76eedfe1d818a19571d33
cedyoung <160330275+cedyoung@users.noreply.github.com>
2026-07-06 22:11:37 +0800

    updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  site: 'https://cedyoung.github.io',
+  base: '/CSARCH2-Virtual-Exhibit-Group-1',
   integrations: [react(), mdx()],
   // Static build is all this exhibit needs — no server runtime required.
   output: 'static',
 });
+
